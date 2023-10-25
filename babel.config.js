382fc336bccfe32bb45c654fcf570a167efdea03
Austin <amartinez15@my.hpu.edu>
2023-10-25 07:28:35 +0000

    Update Gemfile for Heroku

--- babel.config.js
+++ babel.config.js
@@ -54,8 +54,10 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods'
-    
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
       ],
       [
         '@babel/plugin-proposal-private-property-in-object',
