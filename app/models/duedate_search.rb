a343bcb84aacfcb5d880d902e37fcc36bbb658a2
Austin <amartinez15@my.hpu.edu>
2023-10-26 01:04:21 +0000

    Finished

--- app/models/duedate_search.rb
+++ app/models/duedate_search.rb
@@ -3,17 +3,17 @@ class DuedateSearch
 
     def initialize(params)
         params ||= {}
-        @date_from = parsed_date(params[:date_from],7.days.ago.to_date.to_s)
-        @date_to = parsed_date(params[:date_to], Date.today.to_s)
+        @date_from = parsed_date(params[:date_from],30.days.ago.to_date.to_s)
+        @date_to = parsed_date(params[:date_to], 90.days.from_now.to_date.to_s)
     end
 
     def scope 
-        Task.where('date BETWEEN ? AND ?', @date_from, @date_to)
+        Task.where('duedate BETWEEN ? AND ?', @date_from, @date_to)
     end
     
     private
 
-    def parsed_date(date_string,default)
+    def parsed_date(date_string, default)
         Date.parse(date_string)
     rescue ArgumentError,TypeError
         default
